4abf5a9807aa4aa3a391acdfde052a27625ac1fd
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-12 11:50:18 -0500

    Using the useEffect hook to fetch my data and i look through the responseData object and push the id, title and amount into my loadedIngredients array and have no dependencies so the useEffect hook will only run once

--- morehookspractice/src/components/ingredients/Ingredients.js
+++ morehookspractice/src/components/ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import IngredientForm from '../ingredientform/IngredientForm'
 import Search from '../search/Search'
 import IngredientList from '../ingredientlist/IngredientList'
@@ -8,18 +8,41 @@ const Ingredients = () => {
 
   const [userIngredients, setUserIngredients] = useState([])
 
+  useEffect(() => {
+    fetch('https://react-hooks-practice-6b094-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = []
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients)
+      });
+  }, [])
+
   const handleAddIngredient = (ingredient) => {
     fetch('https://react-hooks-practice-6b094-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
-      body: JSON.stringify({ ingredient }),
+      body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
+      return response.json()
+    }).then(responseData => {
       setUserIngredients(prevIngredients => [
         ...prevIngredients,
-        { id: Math.random().toString(), ...ingredient }
+        { id: responseData.name, ...ingredient }
       ])
     })
   }
+
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);
+
 
   const handleRemoveIngredient = (id) => {
     setUserIngredients((prevIngredients) => prevIngredients.filter((ingredient) => ingredient.id !== id))
